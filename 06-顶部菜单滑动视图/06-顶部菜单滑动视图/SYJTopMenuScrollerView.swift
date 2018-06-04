6b7057b771d7cbcb75a3963202d814068ee13770
JayShang <yijieshang@gmail.com>
2018-06-04 09:56:16 +0800

    test oc -> swift仿写

---
+++ 06-顶部菜单滑动视图/06-顶部菜单滑动视图/SYJTopMenuScrollerView.swift
@@ -0,0 +1,33 @@
+//
+//  SYJTopMenuScrollerView.swift
+//  06-顶部菜单滑动视图
+//
+//  Created by caihongguang on 2018/6/4.
+//  Copyright © 2018年 SYJ. All rights reserved.
+//
+
+import UIKit
+
+class SYJTopMenuScrollerView: UIView {
+
+    let bigScrollerView = UIScrollView()
+    
+    let line = UIView()
+
+    let topView = UIView()
+    
+    let selectIndex: Int = 0
+    
+    let titleArray :Array<Any> = []
+    
+    let count: Int = 0
+    
+    
+//    func initWithFrame(cg) -> sfntInstance{
+//        
+//        
+//    }
+    
+    
+
+}
---
+++ 06-顶部菜单滑动视图/06-顶部菜单滑动视图/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  06-顶部菜单滑动视图
+//
+//  Created by caihongguang on 2018/6/4.
+//  Copyright © 2018年 SYJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+   
+
+}
+
